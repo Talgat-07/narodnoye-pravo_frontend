1a862e6109c9754a98ea27108fe910adc2b7f808
Бермет Усубалиева <usubalievabermet@gmail.com>
2024-11-21 00:12:56 +0600

    fix: add import Link

--- src/widgets/Footer/Footer.jsx
+++ src/widgets/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import { navLinks } from "shared/constants/constants";
-
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
     return (
         <footer>
+
             {
                 navLinks.map(el => (
                     <li key={el.path}>
