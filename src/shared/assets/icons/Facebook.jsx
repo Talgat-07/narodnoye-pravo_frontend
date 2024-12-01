5818b46f47ded6371d1095b142a0761a1eb39775
Aizat00dev <urmanbetowwa0@gmail.com>
2024-12-01 09:30:12 +0600

    feat:added styles

--- src/shared/assets/icons/Facebook.jsx
+++ src/shared/assets/icons/Facebook.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+
 
 export const Facebook = (props) => {
-    const { width = '40', height = '40' } = props
+    const { width='40', height='40' } = props
     return (
         <svg
             width={width}
--- src/widgets/AboutUs/AboutUs.jsx
+++ src/widgets/AboutUs/AboutUs.jsx
@@ -1,20 +1,31 @@
 import { Typography } from 'shared/ui/Typography/Typography';
 import aboutUs from '../../shared/assets/img/aboutUsImg.png'
 
+
 export function AboutUs() {
     return (
         <div>
-            <Typography variant = 'h3' weight = 'bold'>О нас </Typography>
+            <Typography
+                variant='h3' 
+                weight='bold'>
+                    О нас 
+            </Typography>
 
-            <img src={aboutUs}/>
+            <img src={aboutUs} alt='весы, рукопожатие'/>
 
-            <Typography variant = 'p' styleType = 'Medium' weight = 'regular'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Suscipit recusandae cupiditate pariatur exercitationem harum rem explicabo, 
-                ullam vitae ad quos, modi consequatur quo rerum labore tempora veniam velit 
-                blanditiis expedita vel quod! 
-                Qui porro et itaque fugit,
-                 corporis facere mollitia hic! Quibusdam sequi optio consectetur?
-                 Ab hic eligendi cupiditate officia.</Typography>
+            <Typography 
+                variant='p' 
+                styleType='Medium' 
+                weight='regular'>
+
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Suscipit recusandae cupiditate pariatur exercitationem harum rem explicabo, 
+                    ullam vitae ad quos, modi consequatur quo rerum labore tempora veniam velit 
+                    blanditiis expedita vel quod! 
+                    Qui porro et itaque fugit,
+                    corporis facere mollitia hic! Quibusdam sequi optio consectetur?
+                    Ab hic eligendi cupiditate officia.
+            </Typography>
             
         </div>
     );
