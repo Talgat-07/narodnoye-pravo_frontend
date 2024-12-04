f307d0dfefced54656bf85b2dd77d0f93d1f887b
Бермет Усубалиева <usubalievabermet@gmail.com>
2024-12-04 15:55:22 +0600

    fix: add hover ServiceCard, style for NewsCard

--- src/entities/ServiceCard/ServiceCard.jsx
+++ src/entities/ServiceCard/ServiceCard.jsx
@@ -4,17 +4,29 @@ import { Typography } from '../../shared/ui/Typography/Typography';
 import { Link } from 'react-router-dom';
 import { path } from 'shared/constants/constants';
 
-export const ServiceCard = () => {
+export const ServiceCard = ({ id, title, description }) => {
     return (
-        <Link to={path.services}
-            className={s.seviceCard}>
-            <Typography
-                className={s.text}
-                variant='p'
-                styleType='Regular'
-                color='white'>
-                Lorem ipsum dolor sit amet
-            </Typography>
+        <Link to={path.services} className={s.seviceCard}>
+            <div className={s.content}>
+                <Typography
+                    className={s.title}
+                    variant='p'
+                    styleType='Regular'
+                    color='white'
+                >
+                    {title}
+                </Typography>
+                <Typography
+                    className={s.description}
+                    variant='p'
+                    styleType='Little'
+                    color='white'
+                    lineHeight='lineNormal'
+                >
+                    {description}
+                </Typography>
+            </div>
         </Link>
+
     );
 };
--- src/widgets/Service/Service.jsx
+++ src/widgets/Service/Service.jsx
@@ -4,9 +4,57 @@ import { Typography } from 'shared/ui/Typography/Typography';
 import { ServiceCard } from 'entities/ServiceCard/ServiceCard';
 import { Link } from 'react-router-dom';
 import { path } from 'shared/constants/constants';
-import { Arrow } from 'shared/assets/icons/Arrow';
+// import { Arrow } from 'shared/assets/icons/Arrow';
+import { ButtonLink } from 'shared/ui/ButtonLink/ButtonLink';
 
 export const Service = () => {
+    const mockService = [
+        {
+            id: 1,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 2,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 3,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 4,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 5,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 6,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 7,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 8,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+        {
+            id: 9,
+            title: 'Услуга №1',
+            description: "Lorem ipsum dolor sit amet consectetur. Laoreet sed neque commodo turpis gravida tellus nulla lacus. Facilisis proin est in velit blandit varius massa. Leo tellus adipiscing in vulputate.",
+        },
+    ]
     return (
         <section className={s.service}>
             <Typography
@@ -15,21 +63,21 @@ export const Service = () => {
                 color='black'>
                 Услуги
             </Typography>
-            <div className={s.cardWrap}>
+            {/* <div className={s.cardWrap}>
                 {Array.from({ length: 9 }, (_, index) => (
                     <ServiceCard key={index} />
                 ))}
+            </div> */}
+            <div className={s.cardWrap}>
+                {mockService.map(servItem => (
+                    <ServiceCard
+                        key={servItem.id}
+                        title={servItem.title}
+                        description={servItem.description} />
+                ))}
             </div>
             <Link to={path.services}>
-                <Typography
-                    className={s.text}
-                    variant='p'
-                    styleType='Medium'
-                    lineHeight='lineLittle'
-                    color='blue'>
-                    Узнать подробнее
-                    <Arrow />
-                </Typography>
+                <ButtonLink />
             </Link>
         </section>
     );
