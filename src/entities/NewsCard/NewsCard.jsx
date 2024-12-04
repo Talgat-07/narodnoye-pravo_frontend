faee6b942515e04dcb20028928580b11bc76b362
Нуриккод <alibaevnursultan29@gmail.com>
2024-12-04 12:19:01 +0600

    some changes

--- src/entities/NewsCard/NewsCard.jsx
+++ src/entities/NewsCard/NewsCard.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function NewsCard() {
   return (
-    <div>NewsCard</div>
+    <div>
+        <Typografy>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, nemo officia distinctio maxime alias ipsam,
+                cumque molestias suscipit unde enim possimus, sunt vitae sapiente aspernatur. Animi adipisci eligendi error, ratione deleniti 
+                excepturi quam recusandae, enim modi accusamus iusto laboriosam voluptatum, architecto dicta! Voluptate molestias cum aspernatur 
+                debitis modi labore quos!</Typografy>
+            <div></div>
+            <Typograhy>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem officia explicabo ipsum! 
+                Est aliquid saepe temporibus officia placeat repellat quo, praesentium officiis harum eaque, 
+                labore quod delectus, quas pariatur porro.</Typograhy>
+
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem velit laboriosam tempore eligendi qui accusamus
+                 porro dolorum error perferendis eius atque quo necessitatibus quis, sapiente maiores harum voluptas minima quia!
+            </Typography>
+    </div>
   )
 }
 
--- src/widgets/LegislativeNews/LegislativeNews.jsx
+++ src/widgets/LegislativeNews/LegislativeNews.jsx
@@ -3,28 +3,16 @@ import NewsCard from "entities/NewsCard/NewsCard"
 
 
 
-
-
-
-
-export const LegislativeNews = () => {
-    return (
-        <div>
-            <Typography>Новости законодательства</Typography>
+// export const LegislativeNews = () => {
+//     return (
+//         <div>
+//             <Typography>Новости законодательства</Typography>
            
-            <Typografy>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, nemo officia distinctio maxime alias ipsam,
-                cumque molestias suscipit unde enim possimus, sunt vitae sapiente aspernatur. Animi adipisci eligendi error, ratione deleniti 
-                excepturi quam recusandae, enim modi accusamus iusto laboriosam voluptatum, architecto dicta! Voluptate molestias cum aspernatur 
-                debitis modi labore quos!</Typografy>
+//             <NewsCard></NewsCard>
+//             <NewsCard></NewsCard>
+//             <NewsCard></NewsCard>
+//             <NewsCard></NewsCard>
 
-            <Typograhy>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem officia explicabo ipsum! 
-                Est aliquid saepe temporibus officia placeat repellat quo, praesentium officiis harum eaque, 
-                labore quod delectus, quas pariatur porro.</Typograhy>
-
-            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem velit laboriosam tempore eligendi qui accusamus
-                 porro dolorum error perferendis eius atque quo necessitatibus quis, sapiente maiores harum voluptas minima quia!
-            </Typography>
-
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
