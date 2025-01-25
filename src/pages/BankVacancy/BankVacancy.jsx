443e359768b691595271f6633bc635c05d7f0e4a
Aizat00dev <urmanbetowwa0@gmail.com>
2025-01-25 14:02:50 +0600

    adding new file

--- src/pages/BankVacancy/BankVacancy.jsx
+++ src/pages/BankVacancy/BankVacancy.jsx
@@ -1,12 +1,12 @@
 import { Container } from "shared/ui/Container/Container";
-import { VacancyCard } from "shared/ui/VacancyCard/VacancyCard";
+
 
 
 export const BankVacancy = () => {
     return (
         <Container>
             <h1>Вакансии</h1>
-            <SuccessWindow />
+           
         </Container>
     );
 };
