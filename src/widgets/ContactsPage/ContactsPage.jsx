15c2e1a52a14acab89c7459b63b2aff789549a48
Aizat00dev <urmanbetowwa0@gmail.com>
2025-02-05 21:23:21 +0600

    fix: Contact

--- src/widgets/ContactsPage/ContactsPage.jsx
+++ src/widgets/ContactsPage/ContactsPage.jsx
@@ -11,6 +11,8 @@ import styles from "./ContactsPage.module.scss";
 
 
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+console.log("Google Maps API Key:", import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
+
 
 const center = {
     lat: 42.8720,
